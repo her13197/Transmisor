27d2f7f86ee57193c82c6d364aa71db774be4743
her13197 <her13197@uvg.edu.gt>
2017-04-20 08:29:08 -0500

    Add files via upload

---
+++ base.cpp
@@ -0,0 +1,296 @@
+#include "pins.h"
+
+#define interrupts() sei()
+#define noInterrupts() cli()
+#define digitalPinToPort(P) ( pgm_read_byte( digital_pin_to_port_PGM + (P) ) )
+#define digitalPinToBitMask(P) ( pgm_read_byte( digital_pin_to_bit_mask_PGM + (P) ) )
+#define digitalPinToTimer(P) ( pgm_read_byte( digital_pin_to_timer_PGM + (P) ) )
+#define analogInPinToBit(P) (P)
+#define portOutputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_output_PGM + (P))) )
+#define portInputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_input_PGM + (P))) )
+#define portModeRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_mode_PGM + (P))) )
+#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
+#define clockCyclesToMicroseconds(a) ( (a) / clockCyclesPerMicrosecond() )
+#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  #define DEFAULT 0
+  #define EXTERNAL 1
+  #define INTERNAL1V1 2
+  #define INTERNAL INTERNAL1V1
+#elif defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  #define DEFAULT 0
+  #define EXTERNAL 4
+  #define INTERNAL1V1 8
+  #define INTERNAL INTERNAL1V1
+  #define INTERNAL2V56 9
+  #define INTERNAL2V56_EXTCAP 13
+#else  
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
+#define INTERNAL1V1 2
+#define INTERNAL2V56 3
+#else
+#define INTERNAL 3
+#endif
+#define DEFAULT 1
+#define EXTERNAL 0
+#endif
+
+volatile unsigned long timer0_overflow_ct = 0;
+volatile unsigned long timer0_mills = 0;
+static unsigned char timer0_fract = 0;
+
+static void turnOffPWM(uint8_t timer)
+{
+  switch (timer)
+  {
+    #if defined(TCCR1A) && defined(COM1A1)
+    case TIMER1A:   cbi(TCCR1A, COM1A1);    break;
+    #endif
+    #if defined(TCCR1A) && defined(COM1B1)
+    case TIMER1B:   cbi(TCCR1A, COM1B1);    break;
+    #endif
+    #if defined(TCCR1A) && defined(COM1C1)
+    case TIMER1C:   cbi(TCCR1A, COM1C1);    break;
+    #endif
+    
+    #if defined(TCCR2) && defined(COM21)
+    case  TIMER2:   cbi(TCCR2, COM21);      break;
+    #endif
+    
+    #if defined(TCCR0A) && defined(COM0A1)
+    case  TIMER0A:  cbi(TCCR0A, COM0A1);    break;
+    #endif
+    
+    #if defined(TCCR0A) && defined(COM0B1)
+    case  TIMER0B:  cbi(TCCR0A, COM0B1);    break;
+    #endif
+    #if defined(TCCR2A) && defined(COM2A1)
+    case  TIMER2A:  cbi(TCCR2A, COM2A1);    break;
+    #endif
+    #if defined(TCCR2A) && defined(COM2B1)
+    case  TIMER2B:  cbi(TCCR2A, COM2B1);    break;
+    #endif
+    
+    #if defined(TCCR3A) && defined(COM3A1)
+    case  TIMER3A:  cbi(TCCR3A, COM3A1);    break;
+    #endif
+    #if defined(TCCR3A) && defined(COM3B1)
+    case  TIMER3B:  cbi(TCCR3A, COM3B1);    break;
+    #endif
+    #if defined(TCCR3A) && defined(COM3C1)
+    case  TIMER3C:  cbi(TCCR3A, COM3C1);    break;
+    #endif
+
+    #if defined(TCCR4A) && defined(COM4A1)
+    case  TIMER4A:  cbi(TCCR4A, COM4A1);    break;
+    #endif          
+    #if defined(TCCR4A) && defined(COM4B1)
+    case  TIMER4B:  cbi(TCCR4A, COM4B1);    break;
+    #endif
+    #if defined(TCCR4A) && defined(COM4C1)
+    case  TIMER4C:  cbi(TCCR4A, COM4C1);    break;
+    #endif      
+    #if defined(TCCR4C) && defined(COM4D1)
+    case TIMER4D: cbi(TCCR4C, COM4D1);  break;
+    #endif      
+      
+    #if defined(TCCR5A)
+    case  TIMER5A:  cbi(TCCR5A, COM5A1);    break;
+    case  TIMER5B:  cbi(TCCR5A, COM5B1);    break;
+    case  TIMER5C:  cbi(TCCR5A, COM5C1);    break;
+    #endif
+  }
+}
+
+void pinMode(uint8_t pin, uint8_t mode)
+{
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  volatile uint8_t *reg, *out;
+
+  reg = portModeRegister(port);
+  out = portOutputRegister(port);
+
+  if (mode == INPUT) { 
+    uint8_t oldSREG = SREG;
+    *reg &= ~bit;
+    *out &= ~bit;
+    SREG = oldSREG;
+  } else {
+    uint8_t oldSREG = SREG;
+    *reg |= bit;
+    SREG = oldSREG;
+  }
+
+}
+
+void digitalWrite(uint8_t pin, uint8_t val)
+{
+  uint8_t timer = digitalPinToTimer(pin);
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  volatile uint8_t *out;
+
+
+  // If the pin that support PWM output, we need to turn it off
+  // before doing a digital write.
+  if (timer != NOT_ON_TIMER) turnOffPWM(timer);
+
+  out = portOutputRegister(port);
+
+  uint8_t oldSREG = SREG;
+
+  if (val == LOW) {
+    *out &= ~bit;
+  } else {
+    *out |= bit;
+  }
+
+  SREG = oldSREG;
+}
+
+int digitalRead(uint8_t pin)
+{
+  uint8_t timer = digitalPinToTimer(pin);
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+
+  // If the pin that support PWM output, we need to turn it off
+  // before getting a digital reading.
+  if (timer != NOT_ON_TIMER) turnOffPWM(timer);
+
+  if (*portInputRegister(port) & bit) return HIGH;
+  return LOW;
+}
+
+
+uint8_t analog_reference = DEFAULT;
+
+void analogReference(uint8_t mode)
+{
+  // can't actually set the register here because the default setting
+  // will connect AVCC and the AREF pin, which would cause a short if
+  // there's something connected to AREF.
+  analog_reference = mode;
+}
+
+int analogRead(uint8_t pin)
+{
+  uint8_t low, high;
+
+#if defined(analogPinToChannel)
+#if defined(__AVR_ATmega32U4__)
+  if (pin >= 18) pin -= 18; // allow for channel or pin numbers
+#endif
+  pin = analogPinToChannel(pin);
+#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  if (pin >= 54) pin -= 54; // allow for channel or pin numbers
+#elif defined(__AVR_ATmega32U4__)
+  if (pin >= 18) pin -= 18; // allow for channel or pin numbers
+#elif defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
+  if (pin >= 24) pin -= 24; // allow for channel or pin numbers
+#else
+  if (pin >= 14) pin -= 14; // allow for channel or pin numbers
+#endif
+
+#if defined(ADCSRB) && defined(MUX5)
+  // the MUX5 bit of ADCSRB selects whether we're reading from channels
+  // 0 to 7 (MUX5 low) or 8 to 15 (MUX5 high).
+  ADCSRB = (ADCSRB & ~(1 << MUX5)) | (((pin >> 3) & 0x01) << MUX5);
+#endif
+  
+  // set the analog reference (high two bits of ADMUX) and select the
+  // channel (low 4 bits).  this also sets ADLAR (left-adjust result)
+  // to 0 (the default).
+#if defined(ADMUX)
+#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = (analog_reference << 4) | (pin & 0x07);
+#else
+  ADMUX = (analog_reference << 6) | (pin & 0x07);
+#endif
+#endif
+
+  // without a delay, we seem to read from the wrong channel
+  //delay(1);
+
+#if defined(ADCSRA) && defined(ADCL)
+  // start the conversion
+  sbi(ADCSRA, ADSC);
+
+  // ADSC is cleared when the conversion finishes
+  while (bit_is_set(ADCSRA, ADSC));
+
+  // we have to read ADCL first; doing so locks both ADCL
+  // and ADCH until ADCH is read.  reading ADCL second would
+  // cause the results of each conversion to be discarded,
+  // as ADCL and ADCH would be locked when it completed.
+  low  = ADCL;
+  high = ADCH;
+#else
+  // we dont have an ADC, return 0
+  low  = 0;
+  high = 0;
+#endif
+
+  // combine the two bytes
+  return (high << 8) | low;
+}
+
+unsigned long millis()
+{
+  unsigned long m;
+  uint8_t oldSREG = SREG;
+  m = timer0_mills;
+  SREG = oldSREG;
+
+  return m;
+}
+
+unsigned long micros() {
+  unsigned long m;
+  uint8_t oldSREG = SREG, t;
+  
+  m = timer0_overflow_ct;
+#if defined(TCNT0)
+  t = TCNT0;
+#elif defined(TCNT0L)
+  t = TCNT0L;
+#else
+  #error TIMER 0 not defined
+#endif
+
+#ifdef TIFR0
+  if ((TIFR0 & _BV(TOV0)) && (t < 255))
+    m++;
+#else
+  if ((TIFR & _BV(TOV0)) && (t < 255))
+    m++;
+#endif
+
+  SREG = oldSREG;
+  
+  return ((m << 8) + t) * (64 / clockCyclesPerMicrosecond());
+}
+
+void delay(unsigned long ms)
+{
+  uint32_t start = micros();
+
+  while (ms > 0) {
+    while ( ms > 0 && (micros() - start) >= 1000) {
+      ms--;
+      start += 1000;
+    }
+  }
+}
+
+
+
